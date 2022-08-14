dc38cb43da1fba50cd4a30b57b65d34437c5d631
Khomotso Tibane <96890432+KhomotsoTibane@users.noreply.github.com>
2022-08-14 15:41:58 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ function App() {
   const [data, setData] = useState({})
   const [location, setLocation] = useState("")
 
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=356e3bde964b8eb6e1412d5c1c144f23&units=metric`;
+  
 
 
   // search for the city weather once the enter button has been pressed
